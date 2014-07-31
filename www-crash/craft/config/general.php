7feef94f0f6659ee8f32cc2edabe5d90e9add62e
Christopher Plummer <plumcakes@mac.com>
2014-07-31 08:43:28 -0400

    multi environment config

--- www-crash/craft/config/general.php
+++ www-crash/craft/config/general.php
@@ -7,8 +7,20 @@
  * You can see a list of the default settings in craft/app/etc/config/defaults/general.php
  */
 
+
 return array(
-    'devMode' => true,
-    // 'usePathInfo' => true,
-    // 'omitScriptNameInUrls' => true
+
+    '*' => array(
+    ),
+
+    'live' => array(
+        'devMode' => true,
+        'usePathInfo' => true,
+        'omitScriptNameInUrls' => true
+    ),
+
+    'local' => array(
+      'devMode' => true,
+    ),
+
 );
--- www-crash/index.php
+++ www-crash/index.php
@@ -12,3 +12,20 @@ if (!is_file($path))
 }
 
 require_once $path;
+
+switch ($_SERVER['SERVER_NAME'])
+
+{
+    // If the SERVER_NAME variable matches our case,
+    // assign the CRAFT_ENVIRONMENT variable a keyword
+    // that identifies this environment that we can
+    // use in our multi-environment config
+
+    case 'signalintrusion.com' :
+        define('CRAFT_ENVIRONMENT', 'live');
+        break;
+
+    default :
+        define('CRAFT_ENVIRONMENT', 'local');
+        break;
+}
