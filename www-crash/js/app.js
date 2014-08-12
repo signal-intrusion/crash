e0c3582691994a8ed0ca09cd58fb0f89f97c4ccc
Christopher Plummer <plumcakes@mac.com>
2014-08-11 21:52:59 -0400

    track progress

--- www-crash/js/app.js
+++ www-crash/js/app.js
@@ -10,20 +10,13 @@
 
     $(window).load(function(){
 
-        // console.log(asideChangers[1].scene.state());
-
-        if (asideChangers[1] && asideChangers[1].scene.state() === "BEFORE"){
-
-
-
+        if (asideChangers[1] && asideChangers[1].scene.state() === 'BEFORE'){
             setTimeout(function () {
                 $('.aside-container').first().addClass('active');
             }, 100);
         }
 
         loadBookmarks();
-        // console.log(supports_html5_storage());
-        // $('.aside-list').first().addClass('active');
     });
 
     function supports_html5_storage() {
@@ -36,11 +29,10 @@
 
     String.prototype.indexOfEnd = function(string) {
         var io = this.indexOf(string);
-        return io == -1 ? -1 : io + string.length;
+        return io === -1 ? -1 : io + string.length;
     };
 
-    $('.aside-list-label').on('click', function(){
-
+    $('.aside-list').on('click', 'label', function(){
         var $this = $(this);
         $this.parent().children().removeClass('active');
         $this.addClass('active');
@@ -48,41 +40,36 @@
     });
 
     $('.in-line-note').on('click mouseover', function(){
-
         var target,
             $this = $(this),
             get = $.grep(this.className.split(' '), function(v){
                 return v.indexOf('note-') === 0;
             }).join();
         target = get.slice(5);
-
-       $('.' + target).click();
-
-       $('.in-line-note.active').removeClass('active');
-       $this.addClass('active');
-
+        $('.' + target).click();
+        $('.in-line-note.active').removeClass('active');
+        $this.addClass('active');
     });
 
     ///////////////////
     // aside changing
     ///////////////////
 
-    function AsideChanger(_element) {
-
-        this.tween;
-        this.scene;
+    function AsideChanger (_element) {
         this.chapter = $(_element).attr('id');
     }
 
     AsideChanger.prototype.scrollAnimate = function () {
 
-        var _this = this;
-
-        this.scene = new ScrollScene({triggerElement: '#' + _this.chapter, offset: 100 })
+        this.scene = new ScrollScene({triggerElement: '#' + this.chapter, offset: 100 })
                         .on('start', function(e){
 
                             var _target,
-                                direction = e.target.parent().info('scrollDirection');
+                                direction = e.target.parent().info('scrollDirection'),
+                                $trigger = $(this.triggerElement()),
+                                leftHref,
+                                leftTitle;
+
 
                             $('.aside-container').removeClass('active');
 
@@ -96,33 +83,39 @@
                                 _target = 'level-' + number;
                                 $('.' + _target).addClass('active');
                             }
+
+                            console.log($trigger);
+                            leftHref = $trigger.next().attr('id');
+                            leftTitle = $trigger.next().attr('data-name');
+
+                            saveLeftoff(leftHref, leftTitle);
                         })
                         .addTo(controller);
-        // this.scene.addIndicators();
 
         return true;
     };
 
+    function saveLeftoff (leftoffLink, leftoffTitle) {
+        localStorage.setItem('leftoffTitle', leftoffTitle);
+        localStorage.setItem('leftoffLink', leftoffLink);
+    }
+
     $.each($('.aside-trigger'), function() {
-        // console.log(this);
-
         var changer = new AsideChanger(this);
-
-        changer.scrollAnimate();
+        AsideChanger.prototype.scrollAnimate.call(changer);
         asideChangers.push(changer);
-
-
     });
+
+    ///////////////////
+    // left-off
+    ///////////////////
 
 
     ///////////////////
     // bookmarks
     ///////////////////
 
-    var bookmarks = {
-        bookmarkArray: []
-    }
-
+    // add a bookmark
     $('.aside-link-button').on('click', function(){
 
         var mark,
@@ -131,6 +124,7 @@
             $this = $(this),
             localMarks;
 
+        // get the bookmarks from localstorage or if empty create new bookmark object
         if (localStorage.getItem('bookmarks') !== null && localStorage.getItem('bookmarks') !== undefined){
             localMarks = JSON.parse(localStorage.getItem('bookmarks'));
         } else {
@@ -139,20 +133,16 @@
             };
         }
 
-        console.log(localMarks);
-
+        // get the title and link for this new bookmark
         title = $this.attr('name');
         link = $this.attr('href');
-
         mark = {
             'title': title,
             'link': link
         };
 
         localMarks.bookmarkArray.push(mark);
-        // console.log(localMarks);
         localStorage.setItem('bookmarks', JSON.stringify(localMarks));
-        // console.log(localStorage.getItem('bookmarks'));
         bookmarks = localMarks;
 
         loadBookmarks();
@@ -162,18 +152,13 @@
     });
 
     // remove bookmarks
-    $('#bookmark-nav-target').on('click', 'div.remove-bookmark', function(e){
-        console.log('fired' + this);
+    $('#bookmark-nav-target').on('click', 'div.remove-bookmark', function(){
         var $this = $(this),
             mark,
-            localMarks = bookmarks,
-            index;
+            localMarks = bookmarks;
 
         mark = $this.attr('name');
-
-        console.log(localMarks);
         localMarks.bookmarkArray = _.without(localMarks.bookmarkArray, _.findWhere(localMarks.bookmarkArray, { title: mark }));
-        console.log(localMarks);
         localStorage.setItem('bookmarks', JSON.stringify(localMarks));
         bookmarks = localMarks;
         loadBookmarks();
@@ -187,16 +172,14 @@
 
         $target.html(compiledTmpl);
 
-    };
+    }
 
     function loadBookmarks() {
 
         var localMarks;
 
         if (localStorage.getItem('bookmarks') !== null && localStorage.getItem('bookmarks') !== undefined){
-
             localMarks = JSON.parse(localStorage.getItem('bookmarks'));
-
         } else {
             localMarks = {
                 bookmarkArray: [
