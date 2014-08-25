443160b7bfed9af995241d31d0b405823506893e
Christopher Plummer <plumcakes@mac.com>
2014-08-25 12:27:19 -0400

    pacman scene changed

--- www-crash/js/animate-atari.js
+++ www-crash/js/animate-atari.js
@@ -1,33 +1,34 @@
 // animation file for atari story
 
-(function (window, $, _, ScrollMagic, TweenMax, TimelineMax){
+(function(window, $, _, ScrollMagic, TweenMax, TimelineMax) {
     'use strict';
 
     console.log('hey atari');
 
     function preload(arrayOfImages) {
-        $(arrayOfImages).each(function () {
+        $(arrayOfImages).each(function() {
             $('<img/>')[0].src = this;
         });
     }
 
     preload([
-            'images/anim-sprites/ghost.png',
-            'images/anim-sprites/ghost-run.png',
-            'images/anim-sprites/ghost-arcade.png',
-            'images/anim-sprites/ghost-arcade-run.png',
-            'images/anim-sprites/pacman.png',
-            'images/anim-sprites/pacman-arcade.png',
-        ]);
+        'images/anim-sprites/ghost.png',
+        'images/anim-sprites/ghost-run.png',
+        'images/anim-sprites/ghost-arcade.png',
+        'images/anim-sprites/ghost-arcade-run.png',
+        'images/anim-sprites/pacman.png',
+        'images/anim-sprites/pacman-arcade.png',
+    ]);
 
     var controller = new ScrollMagic(),
         wp1pos,
         wp2pos,
         wp3pos,
         wp4pos,
-        wp5pos;
+        wp5pos,
+        wp6pos;
 
-    $(window).ready(function(){
+    $(window).ready(function() {
 
     });
 
@@ -39,6 +40,8 @@
     pacmanElements += '<div class="waypoint" id="pacman-waypoint-3"></div>';
     pacmanElements += '<div class="waypoint" id="pacman-waypoint-4"></div>';
     pacmanElements += '<div class="waypoint" id="pacman-waypoint-5"></div>';
+    pacmanElements += '<div class="waypoint" id="pacman-waypoint-6"></div>';
+
 
     $('#pacman-animation').html($(pacmanElements));
 
@@ -47,6 +50,7 @@
     wp3pos = $('#pacman-waypoint-3').position();
     wp4pos = $('#pacman-waypoint-4').position();
     wp5pos = $('#pacman-waypoint-5').position();
+    wp6pos = $('#pacman-waypoint-6').position();
 
     TweenMax.set('#pacman-sprite');
 
@@ -55,18 +59,27 @@
             top: wp1pos.top,
             left: wp1pos.left,
             ease: Linear.easeNone,
+            onReverseComplete: function() {
+                $('#pacman-sprite').css('-webkit-transform', '');
+                pacmanScene.reverse(true);
+                pacmanScene.enabled(true);
+            },
+            onStart: function() {
+                pacmanScene.reverse(false);
+                pacmanScene.enabled(false);
+            }
         }))
         .add(TweenMax.to('#pacman-sprite', 1, {
             top: wp2pos.top,
             left: wp2pos.left,
             ease: Linear.easeNone,
         }))
-        .add(TweenMax.to('#pacman-sprite', 2, {
+        .add(TweenMax.to('#pacman-sprite', 1, {
             top: wp3pos.top,
             left: wp3pos.left / 2 - 50,
             ease: Linear.easeNone
         }))
-        .add(TweenMax.to('#pacman-sprite', 2, {
+        .add(TweenMax.to('#pacman-sprite', 1, {
             top: wp3pos.top,
             left: wp3pos.left,
             ease: Linear.easeNone,
@@ -77,30 +90,37 @@
                 $('#pacman-sprite').css('content', 'url(images/anim-sprites/pacman-arcade.png)');
             }
         }))
-        .add(TweenMax.to('#pacman-sprite', 2, {
+        .add(TweenMax.to('#pacman-sprite', 1, {
             top: wp4pos.top,
             left: wp4pos.left,
             ease: Linear.easeNone
         }))
-        .add(TweenMax.to('#pacman-sprite', 2, {
+        .add(TweenMax.to('#pacman-sprite', 1, {
             top: wp5pos.top,
             left: wp5pos.left,
-            ease: Linear.easeNone
+            ease: Linear.easeNone,
+        }))
+        .add(TweenMax.to('#pacman-sprite', 1, {
+            top: wp6pos.top,
+            left: wp6pos.left,
+            ease: Linear.easeNone,
+            onComplete: function() {
+                $('#pacman-sprite').css('-webkit-transform', 'scale(-1, 1)');
+                pacmanScene.reverse(true);
+                pacmanScene.enabled(true);
+                // pacmanScene.on();
+            },
+            onStart: function() {
+                pacmanScene.reverse(false);
+            }
         }));
 
     var pacmanScene = new ScrollScene({
-        triggerElement: '#pacman-animation',
-        duration: 700,
-        offset: -200
-    })
-        .on('update', function(e){
-            var dir = e.target.parent().info('scrollDirection');
-            if (dir === 'REVERSE') {
-                $('#pacman-sprite').css('-webkit-transform', 'scale(-1, 1)');
-            } else if (dir === 'FORWARD') {
-                // console.log('hit');
-                $('#pacman-sprite').css('-webkit-transform', '');
-            }
+            triggerElement: '#pacman-animation',
+            offset: -50
+        })
+        .on('start', function() {
+            this.enabled(false);
         })
         .setTween(pacmanTween)
         .addTo(controller);
@@ -114,18 +134,27 @@
             top: wp1pos.top,
             left: wp1pos.left,
             ease: Linear.easeNone,
+            delay: 0.5,
+            onStart: function() {
+                ghostScene.reverse(false);
+            },
+            onReverseComplete: function() {
+                ghostScene.reverse(false);
+                ghostScene.enabled(true);
+                $('#ghost-sprite').removeClass('run');
+            }
         }))
         .add(TweenMax.to('#ghost-sprite', 1, {
             top: wp2pos.top,
             left: wp2pos.left,
             ease: Linear.easeNone,
         }))
-        .add(TweenMax.to('#ghost-sprite', 2, {
+        .add(TweenMax.to('#ghost-sprite', 1, {
             top: wp3pos.top,
             left: wp3pos.left / 2 - 50,
             ease: Linear.easeNone
         }))
-        .add(TweenMax.to('#ghost-sprite', 2, {
+        .add(TweenMax.to('#ghost-sprite', 1, {
             top: wp3pos.top,
             left: wp3pos.left,
             ease: Linear.easeNone,
@@ -136,33 +165,34 @@
                 $('#ghost-sprite').addClass('arcade').removeClass('console');
             }
         }))
-        .add(TweenMax.to('#ghost-sprite', 2, {
+        .add(TweenMax.to('#ghost-sprite', 1, {
             top: wp4pos.top,
             left: wp4pos.left,
             ease: Linear.easeNone
         }))
-        .add(TweenMax.to('#ghost-sprite', 2, {
+        .add(TweenMax.to('#ghost-sprite', 1, {
             top: wp5pos.top,
             left: wp5pos.left,
-            ease: Linear.easeNone
+            ease: Linear.easeNone,
+            onStart: function() {
+                ghostScene.reverse(false);
+            },
+            onComplete: function() {
+                ghostScene.reverse(true);
+                ghostScene.enabled(true);
+                $('#ghost-sprite').addClass('run');
+            }
         }));
 
     var ghostScene = new ScrollScene({
         triggerElement: '#pacman-animation',
-        duration: 690,
-        offset: -130
+        offset: -50
     })
-        .on('update', function(e){
-            var dir = e.target.parent().info('scrollDirection');
-            if (dir === 'REVERSE') {
-                $('#ghost-sprite').css('-webkit-transform', 'scale(-1, 1)').addClass('run');
-            } else if (dir === 'FORWARD') {
-                $('#ghost-sprite').css('-webkit-transform', '').removeClass('run');
-            }
-        })
-        .setTween(ghostTween)
+    .on('start', function() {
+        this.enabled(false);
+    })
+    .setTween(ghostTween)
         .addTo(controller);
-
     ghostScene.addIndicators();
 
 })(window, window.jQuery, window._, window.ScrollMagic, window.TweenMax, window.TimelineMax);
